8d64d22e808eaadb9fe772701c4bcb82d2134e8e
Patrick Murray-John <patrickmjchnm@gmail.com>
2011-10-06 16:37:18 -0400

    Actually adds the files for previous commit

---
+++ controllers/MultiCollectionsController.php
@@ -0,0 +1,12 @@
+<?php
+
+class MultiCollections_MultiCollectionsController extends Omeka_Controller_Action
+{
+    
+    public function init()
+    {
+        $this->_modelClass = 'Collection';
+    }
+    
+    
+}
---
+++ views/admin/multi-collections/browse.php
@@ -0,0 +1,75 @@
+<?php head(array('title'=>'Browse Collections', 'bodyclass'=>'collections')); ?>
+<h1>Browse Collections (<?php echo $total_records; ?> total)</h1>
+<?php if (has_permission('Collections', 'add')): ?>
+    <p id="add-collection" class="add-button"><a href="<?php echo html_escape(uri('collections/add')); ?>" class="add add-collection">Add a Collection</a></p>
+<?php endif; ?>
+
+<div id="primary">
+    <?php echo flash(); ?>
+    <?php if (has_collections()): ?>
+        <div class="pagination"><?php echo pagination_links(); ?></div>
+      <?php if (has_collections_for_loop()): ?>
+        <table id="collections" class="simple" cellspacing="0" cellpadding="0">
+            <thead>
+                <tr>
+                <?php browse_headings(array(
+                    'ID' => 'id',
+                    'Name' => 'name',
+                    'Collectors' => null,
+                    'Date Added' => 'added',
+                    'Total Number of Items' => null
+                )); ?>
+                <?php if (has_permission('Collections', 'edit')): ?>
+                    <th scope="col">Edit?</th>
+                <?php endif; ?>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $key = 0;?>
+        <?php while (loop_collections()): ?>
+        
+            <tr class="collection<?php if(++$key%2==1) echo ' odd'; else echo ' even'; ?>">
+                <td scope="row"><?php echo collection('id');?>
+                </td>
+                <td class="title"><?php echo multicollections_link_to_collection(); ?></td>
+                <td>
+                <?php if (collection_has_collectors()): ?>
+                    <ul>
+                        <li><?php echo collection('Collectors', array('delimiter'=>'</li><li>')); ?></li>
+                    </ul>
+                <?php else: ?>
+                    <ul>
+                        <li>No collectors</li>
+                    </ul>
+                <?php endif; ?>
+                
+                </td>
+                <td><?php if($time = collection('Date Added')):?>
+                    <?php echo date('m.d.Y', strtotime($time)); ?>
+                <?php endif; ?>
+                </td>
+                <td><?php echo multicollections_link_to_items_in_collection(); ?></td>
+                
+                <?php if (has_permission('Collections', 'edit')): ?>
+                <td>
+                    <?php echo link_to_collection('Edit', array('class'=>'edit'), 'edit'); ?>
+                </td>
+                <?php endif; ?>
+            </tr>
+        
+            
+
+        <?php endwhile; ?>
+        </tbody>
+        </table>
+      <?php else: ?>
+        <p>There are no collections on this page.  <?php echo link_to('collections', null, 'View All Collections'); ?></p>
+      <?php endif; ?>
+    <?php else: ?>
+        <p>There are no collections in your archive. <?php if (has_permission('Collections', 'add')): ?>Why don't you <?php echo link_to('collections', 'add', 'add one'); ?>?<?php endif; ?></p>
+        
+    <?php endif; ?>
+    
+    <?php fire_plugin_hook('admin_append_to_collections_browse_primary', $collections); ?>
+</div>
+<?php foot(); ?>
